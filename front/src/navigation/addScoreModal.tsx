d0e20f84bd4d559766f8859dcd65dd3a207f7519
Tommi Linnamaa <tommi.linnamaa@gmail.com>
2021-07-06 21:30:10 +0300

    Form submits result to backend

--- front/src/navigation/addScoreModal.tsx
+++ front/src/navigation/addScoreModal.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button, CheckboxProps, DropdownProps, Form, Header, Modal, Radio } from "semantic-ui-react"
+import { Button, CheckboxProps, DropdownProps, Form, Header, Loader, Modal, Radio } from "semantic-ui-react"
 import { IGameData, IVariantInfo } from "../../../back/data/model.interfaces";
 
 interface IModalProps {
@@ -24,7 +24,9 @@ const league = [
 export const AddScoreModal = (props: IModalProps) => {
 
     const [open, setOpen] = React.useState<boolean>(false);
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
     const [canSubmit, setCanSubmit] = React.useState<boolean>(false);
+    const [scoreValue, setScoreValue] = React.useState<string>('');
     const [formState, setFormState] = React.useState<IGameData>(
         {
             league: '',
@@ -39,6 +41,26 @@ export const AddScoreModal = (props: IModalProps) => {
     React.useEffect(() => {
         setCanSubmit(checkValidForm());
     }, [formState])
+
+    const sendNewScore = () => {
+        console.log("sending data", formState)
+        fetch("/api/scores/newScore", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formState)
+        }).then(() => {
+                setIsLoading(false)
+                setOpen(false)
+            })
+            .catch(err => {
+                alert(err);
+                setIsLoading(false);
+                setOpen(false)
+            })
+        setIsLoading(true);
+    }
 
     const handleRadioChange = (e: React.FormEvent<HTMLInputElement>, value: CheckboxProps) => {
         // Was wondering what is the correct way.
@@ -64,13 +86,11 @@ export const AddScoreModal = (props: IModalProps) => {
         }
     }
 
+    // most retarded check, but hey, atlest it's something
     const isValidString = (val?: string): boolean => {
         return val ? true : false
     }
 
-    const isNumber = (val: number): boolean => {
-        return val ? true : false
-    }
 
     const checkValidForm = (): boolean => {
         if (!isValidString(formState.league)) {
@@ -82,7 +102,7 @@ export const AddScoreModal = (props: IModalProps) => {
         if (!isValidString(formState.representation)) {
             return false;
         }
-        if (!isNumber(formState.score)) {
+        if (isNaN(scoreValue as any)) {
             return false;
         }
         if (!isValidString(formState.variant)) {
@@ -101,91 +121,97 @@ export const AddScoreModal = (props: IModalProps) => {
         trigger={props.openButton}
     >
         <Modal.Header>Lisää tulos</Modal.Header>
-        <Modal.Content>
-            <Form>
-                <Form.Group>
-                    <Form.Select
-                        label='Kaupunki'
-                        required
-                        width='4'
-                        placeholder='Kaupunki'
-                        fluid
-                        selection
-                        options={representation}
-                        onChange={handleDropDownCityChange}
-                    />
-                    <Form.Select
-                        label={'Laji'}
-                        required
-                        width='4'
-                        placeholder='Laji'
-                        fluid
-                        selection
-                        options={props.variants.map(v => {
-                            return { key: v.name, text: v.name, value: v.name }
-                        })}
-                        onChange={handleDropDownVariantChange}
-                    />
-                </Form.Group>
-                <Form.Group inline>
+        {isLoading ? <Loader /> :
+            <>
+                <Modal.Content>
+                    <Form>
+                        <Form.Group>
+                            <Form.Select
+                                label='Kaupunki'
+                                required
+                                width='4'
+                                placeholder='Kaupunki'
+                                fluid
+                                selection
+                                options={representation}
+                                onChange={handleDropDownCityChange}
+                            />
+                            <Form.Select
+                                label={'Laji'}
+                                required
+                                width='4'
+                                placeholder='Laji'
+                                fluid
+                                selection
+                                options={props.variants.map(v => {
+                                    return { key: v.name, text: v.name, value: v.name }
+                                })}
+                                onChange={handleDropDownVariantChange}
+                            />
+                        </Form.Group>
+                        <Form.Group inline>
 
-                    {league.map((o, index) => {
-                        return <Form.Radio
-                            key={index}
-                            control={Radio}
-                            label={o.text}
-                            value={o.value}
-                            checked={formState.league === o.value}
-                            onChange={handleRadioChange}
+                            {league.map((o, index) => {
+                                return <Form.Radio
+                                    key={index}
+                                    control={Radio}
+                                    label={o.text}
+                                    value={o.value}
+                                    checked={formState.league === o.value}
+                                    onChange={handleRadioChange}
+                                />
+                            })}
+                        </Form.Group>
+                        <br />
+                        <Form.Group>
+                            <Form.Input
+                                required
+                                width='6'
+                                label='Pelaajan nimi'
+                                placeholder='Nimi'
+                                onChange={(e) => {
+                                    setFormState({ ...formState, players: { p1: e.target.value } })
+                                }}
+                            />
+
+                            <Form.Input
+                                required
+                                width='3'
+                                label='Tulos'
+                                placeholder='Pisteet'
+                                onChange={(e) => {
+                                    setScoreValue(e.target.value)
+                                    if (!isNaN(scoreValue as any)) {
+                                        const val = Number(e.target.value)
+                                        setFormState({ ...formState, score: val })
+                                    }
+                                }}
+                                // Do error handling correctly with a correct form + validator
+                                error={!(isNaN(scoreValue as any)) ? false : true}
+                            />
+
+                        </Form.Group>
+                        <Header>Todiste tuloksesta (linkki videoon tai Kyykkäliiton kilpailu jossa suoritus tapahtui)</Header>
+                        <Form.Input
+                            required
+                            fluid
+                            placeholder='Linkki tai kilpailun nimi'
+                            onChange={(e) => {
+                                setFormState({ ...formState, proof: e.target.value })
+                            }}
                         />
-                    })}
-                </Form.Group>
-                <br />
-                <Form.Group>
-                    <Form.Input
-                        required
-                        width='6'
-                        label='Pelaajan nimi'
-                        placeholder='Nimi'
-                        onChange={(e) => {
-                            setFormState({ ...formState, players: { p1: e.target.value } })
-                        }}
-                    />
+                    </Form>
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button color='blue' onClick={() => setOpen(false)}>
+                        EIKU
+                    </Button>
+                    <Button color='green' disabled={!canSubmit} onClick={() =>  sendNewScore()}>
+                        Lisää
+                    </Button>
+                </Modal.Actions>
+            </>
+        }
 
-                    <Form.Input
-                        required
-                        width='3'
-                        label='Tulos'
-                        placeholder='Pisteet'
-                        onChange={(e) => {
-                            setFormState({ ...formState, score: Number(e.target.value) })
-                        }}
-                    //error={'Numero vaadittu'}
-                    />
-
-                </Form.Group>
-                <Header>Todiste tuloksesta (linkki videoon tai Kyykkäliiton kilpailu jossa suoritus tapahtui)</Header>
-                <Form.Input
-                    required
-                    fluid
-                    placeholder='Linkki tai kilpailun nimi'
-                    onChange={(e) => {
-                        setFormState({ ...formState, proof: e.target.value })
-                    }}
-                />
-            </Form>
-        </Modal.Content>
-        <Modal.Actions>
-            <Button color='blue' onClick={() => setOpen(false)}>
-                EIKU
-            </Button>
-            <Button
-                color='green'
-                disabled={!canSubmit}
-                onClick={() => setOpen(false)}
-            >
-                Lisää
-            </Button>
-        </Modal.Actions>
     </Modal>
 }
