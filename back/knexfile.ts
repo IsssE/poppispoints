d645e5ec9376acd21fc0f95b1731cccce67b9420
Tommi Linnamaa <tommi.linnamaa@gmail.com>
2021-11-13 22:12:12 +0200

    added secret values for knexfile

--- back/knexfile.ts
+++ back/knexfile.ts
@@ -1,7 +1,14 @@
+import {login } from "./secrets"
 module.exports = {
   development: {
     client: 'pg',
-    connection:'postgres://localhost/popoi',
+    //connection:'postgres://localhost/popoi',
+    connection : {
+      host : login.host,
+      user : login.user,
+      password : login.password,
+      database : login.database
+    },
     migrations: {
       directory: './db/migrations'
     },
