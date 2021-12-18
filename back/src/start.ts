36fc5896866233c71d606bb626f6ba001ed43b2e
Tommi Linnamaa <tommi.linnamaa@gmail.com>
2021-12-18 14:22:33 +0200

    big maybe works. front 'dockerized'

--- back/src/start.ts
+++ back/src/start.ts
@@ -5,10 +5,11 @@ import logger from 'loglevel';
 import { getRoutes } from "./routes";
 var bodyParser = require("body-parser");
 
+
 const startServer = (): Promise<Server> => {
 
     const app: Application = express();
-    const port = 8080; // default port to listen
+    const port = process.env.SERVER_PORT || 8080; // default port to listen
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use('/api', getRoutes())  
